ae6cc42d7dc105f684543db03c2709cb1b4b7ab3
Kirill <kirill_1131@mail.ru>
2022-07-03 23:34:04 +0300

    Updates

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export * from "../components/Header/index";
-export * from "../components/Charts/index";
-export * from "../components/Footer/index";
-export * from "../components/Balance/index";
-export * from "../components/Modals/index";
--- src/panels/index.js
+++
@@ -1,6 +0,0 @@
-export * from "../panels/Dashboard/index";
-export * from "../panels/Exchange/index";
-export * from "../panels/ProjectInfo/index";
-export * from "../panels/Wallet/index";
-export * from "../panels/CoinInfo/index";
-export * from "../panels/CreateCoin/index";
